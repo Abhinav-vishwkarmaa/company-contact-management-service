3cb73670e1f5f77947e282a226ce5cdaeef8e799
Abhinav-vishwkarmaa <abhivishwkarmaa52@gmail.com>
2025-11-29 14:07:53 +0530

    fix: explicit pg dialect module for vercel

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,10 @@
 const { Sequelize } = require('sequelize');
 
+const pg = require('pg');
+
 const sequelize = new Sequelize(process.env.DB_URL, {
     dialect: 'postgres',
+    dialectModule: pg,
     protocol: 'postgres',
     logging: false,
     dialectOptions: {
