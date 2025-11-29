55132dca74055a3ca634e42eb7887c22b8fbdcb5
Abhinav-vishwkarmaa <abhivishwkarmaa52@gmail.com>
2025-11-29 14:18:22 +0530

    fix: remove incompatible app.options wildcard for express 5

--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
-app.options("*", cors());
+// app.options("*", cors()); // Removed due to Express 5 incompatibility with '*' wildcard
 
 app.use(helmet());
 
